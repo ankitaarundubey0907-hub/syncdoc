b9df44635ed01f9570b044a81cb1550ce842a14b
keni shah <shahkeni293@gmail.com>
2026-07-15 22:32:43 +0530

    Setup React frontend and create SyncDoc landing page

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>📄 SyncDoc</h1>
+      <h2>Collaborative Document Engine</h2>
+      <p>Welcome to SyncDoc Frontend</p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
