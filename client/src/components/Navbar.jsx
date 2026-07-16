83bf6d3d9818753bd231052b7aacfe277306d77d
Hetvi shah <hetvishah629@gmail.com>
2026-07-16 09:23:40 -0700

    Create home page with hero and feature components

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,42 @@
 import { Link } from "react-router-dom";
-import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="logo">SyncDoc</div>
+    <nav className="bg-white shadow-md">
 
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>
-      </ul>
+      <div className="max-w-7xl mx-auto flex justify-between items-center px-8 py-5">
+
+        <h1 className="text-3xl font-bold text-blue-600">
+          SyncDoc
+        </h1>
+
+        <div className="flex gap-8 text-lg">
+
+          <Link
+            to="/"
+            className="hover:text-blue-600 font-medium"
+          >
+            Home
+          </Link>
+
+          <Link
+            to="/login"
+            className="hover:text-blue-600 font-medium"
+          >
+            Login
+          </Link>
+
+          <Link
+            to="/register"
+            className="bg-blue-600 text-white px-5 py-2 rounded-lg hover:bg-blue-700"
+          >
+            Register
+          </Link>
+
+        </div>
+
+      </div>
+
     </nav>
   );
 }
